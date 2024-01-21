2f605a67eb566035e12731f56617d9ff360c232d
Erik Rose <40496972+erikrose100@users.noreply.github.com>
2024-01-20 19:14:06 -0500

    init commit (#1)

---
+++ Timer/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+
+public class Timer
+{
+    public static void Main()
+    {
+        DateTime localDate = DateTime.Now;
+        DateTime utcDate = DateTime.UtcNow;
+
+        Console.WriteLine($"{CultureInfo.CurrentCulture.NativeName}:");
+        Console.WriteLine($"\tLocal date and time: {localDate.ToString("s", CultureInfo.CurrentCulture)}, {localDate.Kind}");
+        Console.WriteLine($"\tUTC date and time: {utcDate.ToString("s", CultureInfo.CurrentCulture)}, {utcDate.Kind}");
+    }
+}
